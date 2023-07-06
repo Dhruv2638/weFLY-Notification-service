ba982ecb32e6cb868ef3848a44f5048a8d74be6c
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-07-06 22:17:05 +0530

    Added nodemailer and tried to sent a mail

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const { ServerConfig, Logger } = require('./config/index.js');
-const apiRoutes = require('./routes/index.js');
+const express = require("express");
+const { ServerConfig, Logger } = require("./config/index.js");
+const apiRoutes = require("./routes/index.js");
 
+const mailSender = require("./config/email-config.js");
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`successfully server started on port: ${ServerConfig.PORT}`);
-    Logger.info("successfully started the server", "root", {});
-}); 
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, async () => {
+  console.log(`successfully server started on port: ${ServerConfig.PORT}`);
+  try {
+    const responce = await mailSender.sendMail({
+      from: ServerConfig.GMAIL_EMAIL,
+      to: "dhruvnagar2021@gmail.com",
+      subject: "This is temp email with nodemailer service",
+      text: "Yes it is working",
+    });
+    console.log(responce);
+  } catch (error) {
+    console.log(error);
+  }
+});
